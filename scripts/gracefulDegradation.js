796a5047c03544dc368a235868f8435f30a74bf7
Destiny130 <destiny1307654641@hotmail.com>
2018-02-04 22:58:46 +0800

    graceful degradation

---
+++ scripts/gracefulDegradation.js
@@ -0,0 +1,24 @@
+﻿function popUp(winURL)
+{
+    window.open(winURL, "popup", "width=320,height=480");
+}
+
+window.onload = prepareLinks;
+
+function prepareLinks()
+{
+    if (!document.getElementsByTagName) return false;
+    var links = document.getElementsByTagName("a");
+    var i = 0, len = links.length;
+    for (; i < len; i++) {
+        if (links[i].getAttribute("class") == "popup") {
+            console.log(this);  //this is window
+            links[i].onclick = function ()
+            {
+                console.log(this);  //this is 'a'
+                popUp(this.getAttribute("href"));
+                return false;
+            }
+        }
+    }
+}
