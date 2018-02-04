08c471a1ddb6691d596563e3ab2f3d0b7a715ab7
Destiny130 <destiny1307654641@hotmail.com>
2018-02-04 21:49:45 +0800

    update

---
+++ Chapter04/scripts/showPic.js
@@ -0,0 +1,20 @@
+﻿function showPic(whichpic)
+{
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src", source);
+
+    var text = whichpic.getAttribute("title");
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue = text;
+    console.log(description.nodeType);
+    console.log(description.firstChild.nodeType);
+}
+
+function CountBodyChildren()
+{
+    var bodyElement = document.getElementsByTagName("body")[0];
+    console.log(bodyElement.childNodes.length);
+}
+
+window.onload = CountBodyChildren;
